d12fc27a6df4714f24457ecfe62ff9c251f0cba8
kalil <kalil.pelissier@etu.umontpellier.fr>
2019-06-10 01:20:36 +0200

    Mise en place du menu sans arrondis sur les coins supérieurs dû a un fond de couleur blanc

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.myapp;
 
 import android.animation.ArgbEvaluator;
 import android.content.Intent;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private BottomSheetDialog bottomSheetDialog;
+    ImageButton menu;
     TextView counter;
     ImageButton cart;
     ViewPager viewPager;
@@ -26,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        this.cart = findViewById(R.id.cart);
+        this.menu = findViewById(R.id.menu);
 
         models = new ArrayList<>();
         models.add(new Model(R.drawable.parasol, "Parasol", "Beate licentia non Simonides ubi reputantium ac hic patriam ad ac enim ubi ante perfecta."));
@@ -52,10 +61,14 @@ public class MainActivity extends AppCompatActivity {
 
                 if(i < (adapter.getCount() - 1) && i<(colors.length - 1)) {
                     viewPager.setBackgroundColor((Integer)argbEvaluator.evaluate(v, colors[i], colors[i + 1]));
+                    menu.setBackgroundColor((Integer)argbEvaluator.evaluate(v, colors[i], colors[i + 1]));
+                    /*cart.setBackgroundColor((Integer)argbEvaluator.evaluate(v, colors[i], colors[i + 1]));*/
                 }
 
                 else {
                     viewPager.setBackgroundColor(colors[colors.length - 1]);
+                    menu.setBackgroundColor(colors[colors.length - 1]);
+                    /*cart.setBackgroundColor(colors[colors.length - 1]);*/
                 }
             }
 
@@ -71,8 +84,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        this.cart = findViewById(R.id.cart);
-
         cart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -80,6 +91,53 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(cartActivity);
             }
         });
+
+        //ouverture du bottom Sheet menu
+        bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+        View bottomSheetDialogView = getLayoutInflater().inflate(R.layout.dialog_layout, null);
+        bottomSheetDialog.setContentView(bottomSheetDialogView);
+
+        View shareview = bottomSheetDialogView.findViewById(R.id.share);
+        View getLinkview = bottomSheetDialogView.findViewById(R.id.get_link);
+        View editNameview = bottomSheetDialogView.findViewById(R.id.edit);
+        View deletview = bottomSheetDialogView.findViewById(R.id.delet);
+
+        shareview.setOnClickListener(this);
+        getLinkview.setOnClickListener(this);
+        editNameview.setOnClickListener(this);
+        deletview.setOnClickListener(this);
+
+        ImageButton button = findViewById(R.id.menu);
+        button.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.menu:
+                bottomSheetDialog.show();
+                break;
+
+            case R.id.share:
+                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.get_link:
+                Toast.makeText(MainActivity.this, "Get Link", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.edit:
+                Toast.makeText(MainActivity.this, "Edit Name", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.delet:
+                Toast.makeText(MainActivity.this, "Delete collection", Toast.LENGTH_SHORT).show();
+                break;
+        }
 
     }
 
